15e0c2929f2195deb6b83775d0adae8f8f8e4f69
Max <maxkuznec12@gmail.com>
2020-04-21 20:01:02 +0300

    исправил

--- script_today/today.js
+++ script_today/today.js
@@ -9,20 +9,20 @@ setInterval(function() {
         let result;
 
         if(hour >= 5 && hour <= 9) {
-            result = 'утро';
+            result = 'Доброе утро';
         } else if (hour > 9 && hour <= 17) {
-            result = 'день';
+            result = 'Добрый день';
         } else if (hour > 17 && hour <= 21) {
-            result = 'вечер';
+            result = 'Добрый вечер';
         } else {
-            result = 'ночь';
+            result = 'Доброй ночи';
         }
 
         return result;
     } 
 
     function setNum(num){
-        if (num > 0 && num < 10) { 
+        if (num >= 0 && num < 10) { 
             return '0' + num;
         } else {
             return num;
@@ -37,7 +37,7 @@ setInterval(function() {
         return daysLeft;
     }
     
-    elements += '<p>Добрый ' + howTime(date.getHours()) + '</p>';
+    elements += '<p>' + howTime(date.getHours()) + '</p>';
     elements += '<p>Сегодня: ' + days[date.getDay()] + '</p>';
     elements += '<p>Текущее время: ' + setNum(date.getHours()) + ':' + setNum(date.getMinutes()) + ':' + setNum(date.getSeconds()) + ' ' + checkTime + '</p>';
     elements += '<p>До нового года осталось ' + daysLeftNewYear() + ' дней</p>';
