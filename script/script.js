33838666de046af45cd112622947142b90183d3b
Max <maxkuznec12@gmail.com>
2020-04-25 18:21:02 +0300

    вернул анимацию

--- script/script.js
+++ script/script.js
@@ -83,14 +83,28 @@ window.addEventListener('DOMContentLoaded', function(){
     const togglePopUp = () => {
         let popup = document.querySelector('.popup'),
             popupBtn = document.querySelectorAll('.popup-btn'),
-            popupContent = document.querySelector('.popup-content');
+            popupContent = document.querySelector('.popup-content'),
+            animateInterval,
+            countTop = -382,
+            animate = false;
+
+        const popupAnimate = () => {
+            animateInterval = requestAnimationFrame(popupAnimate);
+            countTop++;
+
+            if(countTop < 100) {
+                popupContent.style.top = countTop + 'px';
+            } else {
+                cancelAnimationFrame(animateInterval);
+            }
+        }
 
         popupBtn.forEach((elem) => {
             elem.addEventListener('click', () => {
                 if(screen.width > 768) {
-                    setTimeout(function() {
-                        popup.style.display = 'block';
-                    }, 1000);
+                    popupContent.style.top = `-382px`;
+                    popup.style.display = 'block';
+                    animateInterval = requestAnimationFrame(popupAnimate);
                 } else {
                     popup.style.display = 'block';
                 }
@@ -102,11 +116,13 @@ window.addEventListener('DOMContentLoaded', function(){
 
             if(target.classList.contains('popup-close')) {
                 popup.style.display = 'none';
+                countTop = -382;
             } else {
                 target = target.closest('.popup-content');
 
                 if(!target) {
                     popup.style.display = 'none';
+                    countTop = -382;
                 }
             }
         });
